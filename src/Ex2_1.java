75d1b2e1546669841ef2791151b54e5ef93e5ed3
rlagu <rlagu@192.168.219.192>
2021-12-30 13:58:06 +0900

    제발루2

---
+++ src/Ex2_1.java
@@ -0,0 +1,21 @@
+
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Ctrl+alt+down == 행단위 전체 복사
+		//print() ==줄 바꿈 없음 
+		//println()==print 후 줄까지 바꿔주기
+		//ctrl+Space== 자동 완성
+		// alt+ shift+A == 여러줄에 걸친 수정 + shift + 화살표
+		System.out.println();
+		System.out.println(5*3);
+		System.out.println(5/3.00);
+		System.out.println(5%3);
+		System.out.println(5-3);
+		
+		
+	}
+
+}
